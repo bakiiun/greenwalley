0131a327901a43d557f9cd2b92337587c4f92925
bakiun <baki94@gmail.com>
2022-04-15 15:59:48 +0300

    added custom messages

--- API/src/middleware/validate_payload.ts
+++ API/src/middleware/validate_payload.ts
@@ -7,18 +7,20 @@ export function verify(rules: {}) {
       const reqBody = new clientDTO(req.body);
       const keys = Object.keys(rules);
       const parameters = keys.reduce((i, j) => ({ ...i, [j]: reqBody[j] }), {});
-
       const bodyRules = Joi.object(rules);
       await bodyRules.validateAsync(parameters, {
         //* CUSTOM ERROR MESSAGES
-        //   messages: {
-        //     "string.empty": "Zorunlu alanlar boş bırakılamaz.",
-        //     "string.min": "Zorunlu alanlar en az 4 karakterden oluşmalı.",
-        //   },
+        messages: {
+          "string.empty": "Zorunlu alanlar boş bırakılamaz.",
+          "string.min": "Zorunlu alanlar belirtilen uzunlukta değil.",
+          "string.max": "Zorunlu alanlar belirtilen uzunlukta değil.",
+          "number.base": "Zorunlu alanlar uygun doldurulmalı.",
+        },
       });
       next();
-    } catch (error) {
-      res.status(400).json({ message: "İstenilen alanları uygun şekilde doldurunuz." });
+    } catch (error: any) {
+      console.log(error);
+      res.status(400).json({ message: error.details[0].message });
     }
   };
 }
