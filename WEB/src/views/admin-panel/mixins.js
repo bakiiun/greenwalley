6c970eb20de0ab5fab5fa02bb2dd010ab6e07584
bakiun <baki94@gmail.com>
2022-04-18 02:10:56 +0300

    updated notification event

--- WEB/src/views/admin-panel/mixins.js
+++ WEB/src/views/admin-panel/mixins.js
@@ -2,9 +2,14 @@ import { api } from "../../plugins/axios/Req-Module";
 export default {
   methods: {
     mPersonalInfo() {
-      api("get", "/gate").then((res) => {
-        this.$data.personalInfo = res.data;
-      });
+      api("get", "/gate")
+        .then((res) => {
+          this.$data.personalInfo = res.data;
+        })
+        .catch(() => {
+          this.notification(["Yönlendiriliyor", "Lütfen giriş yapınız.", "warning"]);
+          this.$router.push("/login");
+        });
     },
     async mPersonalUpdate(payload) {
       return api("patch", "/personals", payload).then((res) => {
