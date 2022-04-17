68177fb02292412565b65d7861b5b1a49dc6ad3e
bakiun <baki94@gmail.com>
2022-04-18 02:11:32 +0300

    updated notification event

--- WEB/src/views/login/mixins.js
+++ WEB/src/views/login/mixins.js
@@ -1,20 +1,30 @@
 import { api } from "../../plugins/axios/Req-Module";
+import { mapActions } from "vuex";
 
 export default {
   methods: {
+    ...mapActions(["notification"]),
+
     mLogin() {
       const { username, pwd } = this.$data;
-      api("post", "/login", { username, pwd }).then(async (i) => {
-        localStorage.setItem("token", i.data.token);
-        await this.$router.push("/dashboard");
-        return;
-      });
+      api("post", "/login", { username, pwd })
+        .then(async (i) => {
+          if (i.status == 200) {
+            localStorage.setItem("token", i.data.token);
+            await this.$router.push("/dashboard");
+            this.notification(["Giriş Başarılı", null, "success"]);
+          }
+        })
+        .catch(() => {
+          this.$data.pwd = "";
+        });
     },
   },
 
   mounted() {
     api("get", "/gate").then((i) => {
       if (i.status == 200) {
+        this.notification(["Yönlendiriliyor", "Zaten giriş yapılmış.", "warning"]);
         this.$router.push("/dashboard");
       }
     });
